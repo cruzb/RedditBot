51e28551de45834e7cbda95f2b51d51f528c2d1d
cruzb <cruzb1010@gmail.com>
2018-09-30 03:07:14 -0400

    optimization
    Removed several data points, and function calls that would have triggered unnecessary calls to the reddit API. Total improvement around 100% faster.

--- comments.py
+++ comments.py
@@ -12,22 +12,24 @@ def main():
 
     db = sqlite3.connect('data.sqlite')
     cursor = db.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS comments (id TEXT PRIMARY KEY, comment_id TEXT, host TEXT, url TEXT, subreddit TEXT, score INTEGER, gilds INTEGER, time INTEGER, author TEXT, nsfw INT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS comments (id TEXT PRIMARY KEY, comment_id TEXT, host TEXT, url TEXT, subreddit_id TEXT, score INTEGER, gilds INTEGER, time INTEGER, author TEXT, nsfw INT)')
     print('---Init Complete---\n\n')
 
-    comment_count = 0.0
-    total_time = 0.0
-    total_count = 0
+
+    start_time = time.time()
+    count = 0
     try:
-        while True:
-            print('----------Begin one loop----------')
-            start_time = time.time()
-            handle_comments(db, cursor, subreddit)
-            print('Comments execution time: ' + str(time.time()-start_time))
-            comment_count += 1.0
-            total_time += time.time()-start_time
-            print('Comments average execution time: ' + str(total_time/comment_count))
-            print('----------Completed one loop----------\n\n')
+        for comment in subreddit.stream.comments():
+            handle_comment(db, cursor, comment)
+            count += 1
+            print('one done')
+
+            if(count == 100):
+                count = 0
+                print('----------Completed 100 Comments----------')
+                print('Average Execution time: ' + str((time.time()-start_time)/100))
+                print('\n\n')
+                start_time = time.time()
     #end program gracefully if closed with console ctrl+c
     except KeyboardInterrupt:
         db.commit()
@@ -36,49 +38,40 @@ def main():
 
 
 
-def handle_comments(db, cursor, subreddit):
-    print('---Getting Comments---')
-    count = 0
-    comments = subreddit.comments(limit=100)
-    for comment in comments:
-        text = comment.body
+def handle_comment(db, cursor, comment):
+    text = comment.body
 
-        cursor.execute('SELECT * FROM comments WHERE comment_id = ?', (comment.id,))
-        rows = cursor.fetchall()
-        if(len(rows) > 0):
-            continue
+    cursor.execute('SELECT * FROM comments WHERE comment_id = ?', (comment.id,))
+    rows = cursor.fetchall()
+    if(len(rows) > 0):
+        return
 
-        #find urls in text if any exist
-        urls = re.findall('(http[s]?://[^\s]+)', text)
-        for url in urls:
-            #There can be trailing ) ] > due to reddit formatting, cut them here
-            if(url[len(url) - 1] == ')' or url[len(url) - 1] == ']' or url[len(url) - 1] == '>'):
-                url = url[:-1]
+    #find urls in text if any exist
+    urls = re.findall('(http[s]?://[^\s]+)', text)
+    for url in urls:
+        #There can be trailing ) ] > due to reddit formatting, cut them here
+        url.rstrip('*]>)')
 
-            #parse url to get host
-            try:
-                parsed_url = urlparse(url)
-                #remove subdomain because urlparse gives with it
-                #hostname = parsed_url.hostname.split('.', 1)[1]
-                hostname = parsed_url.hostname
-                if(hostname.startswith('www.')):
-                    hostname = hostname[4:]
+        #parse url to get host
+        try:
+            parsed_url = urlparse(url)
+            #remove subdomain because urlparse gives with it
+            #hostname = parsed_url.hostname.split('.', 1)[1]
+            hostname = parsed_url.hostname
+            if(hostname.startswith('www.')):
+                hostname = hostname[4:]
 
-                #generate unique id to prevent same links from same comment being added multiple times
-                id = comment.id + "-" + url
-                #store entry or ignore if one already exists
-                cursor.execute('INSERT OR IGNORE INTO comments (id, comment_id, host, url, subreddit, score, gilds, time, author, nsfw) VALUES (?,?,?,?,?,?,?,?,?,?)',
-                                (id, comment.id, hostname, url, comment.subreddit.display_name, comment.score, comment.gilded, round(comment.created_utc),
-                                comment.author.name, comment.submission.over_18))
-            except ValueError:
-                print('ERROR: Invalid url - ' + url)
+            #generate unique id to prevent same links from same comment being added multiple times
+            id = comment.id + "-" + url
+            #store entry or ignore if one already exists
+            cursor.execute('INSERT OR IGNORE INTO comments (id, comment_id, host, url, subreddit_id, score, gilds, time, author, nsfw) VALUES (?,?,?,?,?,?,?,?,?,?)',
+                            (id, comment.id, hostname, url, comment.subreddit_id, comment.score, comment.gilded, round(comment.created_utc),
+                            comment.author.name, comment.submission.over_18))
+        except ValueError:
+            print('ERROR: Invalid url - ' + url)
 
 
-        db.commit()
-        count += 1
-
-
-    print('Processed ' + str(count) + ' comments')
+    db.commit()
 
 
 
